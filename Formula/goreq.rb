6a6f179c0571a49501d9da953dd64560146eddba
eddietsai <vvvdwbvvv@gmail.com>
2025-02-05 23:03:53 +0800

    Add Homebrew formula for goreq

--- Formula/goreq.rb
+++ Formula/goreq.rb
@@ -2,7 +2,7 @@ class Goreq < Formula
     desc "A fast and lightweight HTTP client written in Go"
     homepage "https://github.com/vvvdwbvvv/goreq"
     version "1.0.0"
-    url "https://github.com/vvvdwbvvv/goreq/releases/v1.0.0/goreq-darwin-arm64.tar.gz"
+    url "https://github.com/vvvdwbvvv/goreq/releases/download/v1.0.0/goreq-darwin-arm64.tar.gz"
     sha256 "e76272f9f6e0df22ef53830bdccfaa4442046902302f7694378ec45a8f7e31d2  goreq-darwin-arm64.tar.gz"
     def install
         bin.install "goreq"
